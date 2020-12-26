7f447e6c4c49c6c09fc1be79d2a6d9bd82d494c9
Stanislav.Ivanov <stanislav_ivanov9213@abv.bg>
2020-12-26 13:21:47 +0200

    procfile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,38 +12,37 @@ class UserController extends Controller
     //
   public function Login(Request $request)
    {
-    $email =$request->input('email');
-    $password=$request->input('password');
-    $token="";
-    if(Auth::attempt(['email'=>$email,'password'=>$password]))
-     {
-      $token = $request->user()->currentAccessToken();
-      return response()->json(['email'=>$email,'password'=>$password,'token'=>$token],200);
-     }
+    if ($request->has(['email','password'])) {
+        $email =$request->input('email');
+        $password=$request->input('password');
+        $token="";
+        if (Auth::attempt(['email'=>$email,'password'=>$password])) {
+            $token = $request->user()->currentAccessToken();
+            return response()->json(['email'=>$email,'password'=>$password,'token'=>$token], 200);
+        }
+    }
      return response()->json(['msg'=>'Error'],400);
-
    }  
    public function Register(Request $request)
    {
-    $name = $request->input('name');
-    $email =$request->input('email');
-    $password=$request->input('password');
-    $created_at = now();
-    $user = new User;
-    $user->name = $name;
-    $user->email = $email;
-    $user->created_at = $created_at;
-    $user->password = Hash::make($password);
-    $user->save();
-    $token="";
-    if(Auth::attempt(['email'=>$email,'password'=>$password]))
-     {
-      $token = $request->user()->createToken($request->token_name);
-      return response()->json(['name'=>$name,'email'=>$email,'password'=>$password,'token'=>$token],200);
-
+     if ($request->has(['name','email','password'])) {
+         $name = $request->input('name');
+         $email =$request->input('email');
+         $password=$request->input('password');
+         $created_at = now();
+         $user = new User;
+         $user->name = $name;
+         $user->email = $email;
+         $user->created_at = $created_at;
+         $user->password = Hash::make($password);
+         $user->save();
+         $token="";
+         if (Auth::attempt(['email'=>$email,'password'=>$password])) {
+             $token = $request->user()->createToken($request->token_name);
+             return response()->json(['name'=>$name,'email'=>$email,'password'=>$password,'token'=>$token], 200);
+         }
      }
-     return response()->json(['msg'=>'Error'],400);
-             
+     return response()->json(['msg'=>'Error'],400);            
    }  
 
 }
