9347173d9bf17e346d98737bcd9dae9270989c00
Stanislav.Ivanov <stanislav_ivanov9213@abv.bg>
2021-01-05 17:30:01 +0200

    new req tests 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,8 +16,8 @@ class UserController extends Controller
         $email =$request->input('0.email');
         $password=$request->input('0.password');
         if (Auth::attempt(['email'=>$email,'password'=>$password])) {
-            $token = $request->user()->currentAccessToken();
-            dd($token);
+            $tokens = $request->user()->tokens;
+            dd($tokens[0]);
             return response()->json(['email'=>$email,'password'=>$password,'token'=>$token], 200);
         }
     }
