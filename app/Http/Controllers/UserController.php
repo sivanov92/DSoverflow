e67ceffdbf35055731c97c561bb40abd31830387
Stanislav.Ivanov <stanislav_ivanov9213@abv.bg>
2021-01-05 14:13:13 +0200

    request test 11

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Str;
 class UserController extends Controller
 {
     //
