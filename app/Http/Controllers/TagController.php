222fc4655bf2b95b12752bd4a77b2e26478e767c
Stanislav.Ivanov <stanislav_ivanov9213@abv.bg>
2021-01-31 00:03:28 +0200

    added tags and many to many conn to posts, also tag controller , however more post controller funcs needed to handle tags

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Tag;
+use App\Http\Controllers\ListController;
+
+class TagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $validated = $request->validate([
+            '0.name' => 'required|max:255',
+        ]);
+        if(!$validated){
+            return response('Validator failed',400);
+        }
+
+        //
+      $name = $request->input('0.name') ;
+      $tag = new Tag;
+      $tag -> name = $name ;
+      $tag ->created_at = now();
+      $tag ->save();
+      $cache_key1 = ListController::CACHE_KEY.'.TagList';
+      cache()->forget($cache_key1);
+
+      return response("Success");
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+      return Tag::findOrFail($id);  
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $validated = $request->validate([
+            '0.name' => 'required|max:255',
+        ]);
+        if(!$validated){
+            return response('Validator failed',400);
+        }
+
+        //
+      $name = $request->input('0.name') ;
+      $tag = Tag::findOrFail($id);
+      $tag -> name = $name ;
+      $tag ->save();
+      $cache_key1 = ListController::CACHE_KEY.'.TagList';
+      cache()->forget($cache_key1);
+
+      return response("Success");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+      $tag = Tag::findOrFail($id);
+      $tag -> delete();
+      $cache_key1 = ListController::CACHE_KEY.'.TagList';
+      cache()->forget($cache_key1);
+
+      return response("Tag Deleted");
+  
+    }
+}
