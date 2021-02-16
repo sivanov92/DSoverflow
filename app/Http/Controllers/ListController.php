872369327ca9e779c4d77e2e212995936981cb8c
Stanislav.Ivanov <stanislav_ivanov9213@abv.bg>
2021-02-16 21:52:26 +0200

    no more url paramenters, to be updated

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -12,20 +12,16 @@ class ListController extends Controller
     const CACHE_KEY = 'LIST';
     const CACHE_MINUTES = 5;
     //
-   public function getPostsList( $limit = 10 , $offset = 0 ){
-     $list = Post::offset($offset)
-               -> limit($limit)
-               -> get();
+   public function getPostsList(){
+    $list = Post::all();
     $key = self::CACHE_KEY.".PostList";
     return cache()->remember($key,now()->addMinutes(self::CACHE_MINUTES),function() use ($list){
       return $list;
     });           
 }
 
-   public function getCommentsPerPost($post_id , $limit = 10 , $offset = 0){
+   public function getCommentsPerPost($post_id){
     $list = Comment::where('post_id',$post_id)
-                           -> offset($offset)
-                           -> limit($limit)
                            -> get();  
     $key = self::CACHE_KEY.".CommentsPerPost.".$post_id;
     return cache()->remember($key,now()->addMinutes(self::CACHE_MINUTES),function() use ($list){
@@ -33,10 +29,8 @@ class ListController extends Controller
     });                                                                         
    }
 
-   public function getCommentsPerUser($user_id , $limit = 10 , $offset = 0){
+   public function getCommentsPerUser($user_id){
     $list =  Comment::where('user_id',$user_id)
-                        -> offset($offset)
-                        -> limit($limit)
                         -> get();  
     $key = self::CACHE_KEY.".CommentsPerUser.".$user_id;
     return cache()->remember($key,now()->addMinutes(self::CACHE_MINUTES),function() use ($list){
@@ -44,10 +38,8 @@ class ListController extends Controller
     });                                                                      
    }
 
-   public function getPostsPerUser($user_id , $limit = 10 , $offset = 0){
+   public function getPostsPerUser($user_id){
     $list =  Post::where('user_id',$user_id)
-                        -> offset($offset)
-                        -> limit($limit)
                         -> get();  
     $key = self::CACHE_KEY.".PostsPerUser.".$user_id;
     return cache()->remember($key,now()->addMinutes(self::CACHE_MINUTES),function() use ($list){
@@ -55,34 +47,28 @@ class ListController extends Controller
     });                                                                                                          
    }
    //Tags
-   public function getPostsPerTag($tag_id , $limit = 10 , $offset = 0 ){
+   public function getPostsPerTag($tag_id){
     $posts = Post::all();
     $list = $posts->tags()
               -> where('tag_id' , $tag_id)
-              -> offset($offset)
-              -> limit($limit)
               -> get();
    $key = self::CACHE_KEY.".PostsPerTag.".$tag_id;
    return cache()->remember($key,now()->addMinutes(self::CACHE_MINUTES),function() use ($list){
     return $list;
   });           
 }
-public function getTagsOfPost($post_id , $limit = 10 , $offset = 0 ){
+public function getTagsOfPost($post_id){
   $tags = Tag::all();
   $list = $tags->posts()
             -> where('post_id' , $post_id)
-            -> offset($offset)
-            -> limit($limit)
             -> get();
  $key = self::CACHE_KEY.".TagsPerPost.".$post_id;
  return cache()->remember($key,now()->addMinutes(self::CACHE_MINUTES),function() use ($list){
   return $list;
 });           
 }
-public function getAllTags( $limit = 10 , $offset = 0 ){
-  $list = Tag:: offset($offset)
-            -> limit($limit)
-            -> get();
+public function getAllTags(){
+  $list = Tag::all();
  $key = self::CACHE_KEY.".TagList";
  return cache()->remember($key,now()->addMinutes(self::CACHE_MINUTES),function() use ($list){
   return $list;
--- routes/web.php
+++ routes/web.php
@@ -32,13 +32,13 @@ Route::middleware('check_token')->group(function(){
     'tags'     => TagController::class]);
 });
 
-Route::get('/lists/posts/{limit?}/{offset?}',[ListController::class , 'getPostsList']);                     
-Route::get('/lists/posts/user-id/{user_id}/{limit?}/{offset?}',[ListController::class , 'getPostsPerUser']);                     
-Route::get('/lists/comments/user-id/{user_id}/{limit?}/{offset?}',[ListController::class , 'getCommentsPerUser']);                     
-Route::get('/lists/comments/post-id/{post_id}/{limit?}/{offset?}',[ListController::class , 'getCommentsPerPost']);                     
+Route::get('/lists/posts',[ListController::class , 'getPostsList']);                     
+Route::get('/lists/posts/user-id/{user_id}',[ListController::class , 'getPostsPerUser']);                     
+Route::get('/lists/comments/user-id/{user_id}',[ListController::class , 'getCommentsPerUser']);                     
+Route::get('/lists/comments/post-id/{post_id}',[ListController::class , 'getCommentsPerPost']);                     
 
-Route::get('/lists/tags/{limit?}/{offset?}',[ListController::class , 'getAllTags']);                     
-Route::get('/lists/posts/tag-id/{tag_id}/{limit?}/{offset?}',[ListController::class , 'getPostsPerTag']);                     
-Route::get('/lists/tags/post-id/{post_id}/{limit?}/{offset?}',[ListController::class , 'getTagsOfPost']);                     
+Route::get('/lists/tags',[ListController::class , 'getAllTags']);                     
+Route::get('/lists/posts/tag-id/{tag_id}',[ListController::class , 'getPostsPerTag']);                     
+Route::get('/lists/tags/post-id/{post_id}',[ListController::class , 'getTagsOfPost']);                     
 
 
